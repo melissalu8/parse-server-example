769282950b2e851dece6184dc900dbf86ace3d24
Melissa Lu <melissalu@fb.com>
2018-08-09 19:17:40 -0700

    changes

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,35 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('pingReply', function(request, response) {
+
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var to = JSON.parse(customData).to;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("deviceToken", to);
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true})
+  .then(function() {
+  	// Push sent!
+    console.log("Push to user was successful");
+  }, function(error) {
+  	// There was a problem :(
+    console.log("Error sending push: " + error.code + " - " + error.message);
+  });
+
+  response.success('success');
+
 });
--- index.js
+++ index.js
@@ -19,6 +19,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  push: {
+    android:{
+      senderId:process.env.FCM_SENDER_ID || '',
+      apiKey:process.env.FCM_API_KEY || ''
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
