f85c2db3988e8fbf46a3cd9b61db2672b3ecd65d
Melissa Lu <melissalu@fb.com>
2018-08-10 11:14:30 -0700

    Like Notification

--- cloud/main.js
+++ cloud/main.js
@@ -34,3 +34,41 @@ Parse.Cloud.define('nearby', function(request, response) {
   response.success('success');
 
 });
+
+
+
+Parse.Cloud.define('like', function(request, response) {
+
+  var params = request.params;
+  var likeData = params.likeData;
+
+  if (!likeData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(likeData).sender;
+  var toToken = JSON.parse(likeData).toToken;
+  var  message = JSON.parse(likeData).message;
+  var likeQuery = new Parse.Query(Parse.Installation);
+  likeQuery.equalTo("deviceToken", toToken);
+
+  Parse.Push.send({
+  where: likeQuery,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": message},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true})
+  .then(function() {
+    // Push sent!
+    console.log("Push to user was successful");
+  }, function(error) {
+    // There was a problem :(
+    console.log("Error sending push: " + error.code + " - " + error.message);
+  });
+
+  response.success('success');
+
+});
